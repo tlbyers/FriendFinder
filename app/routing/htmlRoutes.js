00fd2e44a3a3fa3e93ba9bb9435291d68f1c4b51
Terri Byers <Terri Byers>
2017-05-16 20:48:34 -0400

    added default

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,10 +3,10 @@ var path = require("path");
 module.exports=function(app){
 
 	app.get("/survey",function(req,res){
-		res.sendFile(path.join(_dirname+ "/../public/survey.html"));
+		res.sendFile(path.join(_dirname+ "../public/survey.html"));
 	});
 
-	app.use("/home.html",function(req,res){
-		res.sendFile(path.join(__dirname+ "/../public/home.html"));
+	app.use("/",function(req,res){
+		res.sendFile(path.join(__dirname+ "../public/home.html"));
 	});
 }
