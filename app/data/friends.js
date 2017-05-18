be51f9b9426fbee1efd93f9e1a459d501be64ea4
Terri Byers <Terri Byers>
2017-05-17 20:08:14 -0400

    changed questions add friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -16,6 +16,91 @@ var friends=[
         1
       ]
   }
+
+  {
+    name:"Johnny Depp",
+    photo:"https://pbs.twimg.com/profile_images/461110955082924033/syKzaNQA.png",
+    scores:[
+        3,
+        2,
+        5,
+        1,
+        3,
+        1,
+        2,
+        4,
+        2,
+        1
+      ]
+  }
+
+  {
+    name:"Tom Hanks",
+    photo:"http://media.npr.org/assets/img/2016/12/12/tom-hanks---craig-kief_sq-6b0065043c4d147c8bbd82351907660d83676f67-s300-c85.jpg",
+    scores:[
+        3,
+        2,
+        4,
+        5,
+        1,
+        3,
+        4,
+        2,
+        5,
+        
+      ]
+  }
+
+  {
+    name:"Brad Pitt",
+    photo:"http://a57.foxnews.com/images.foxnews.com/content/fox-news/entertainment/2017/05/15/brad-pitt-talks-sober-life-post-angelina-jolie-split-ive-got-no-secrets/_jcr_content/par/featured-media/media-0.img.jpg/876/493/1494878620749.jpg?ve=1&tl=1",
+    scores:[
+        5,
+        5,
+        5,
+        3,
+        2,
+        1,
+        4,
+        5,
+        4,
+        1
+      ]
+  }
+
+  {
+    name:"Bruce Willis",
+    photo:"https://vignette2.wikia.nocookie.net/rugrats/images/9/92/Bruce-willis.jpg/revision/latest?cb=20160321074715",
+    scores:[
+        3,
+        2,
+        5,
+        1,
+        2,
+        4,
+        3,
+        2,
+        4,
+        1
+      ]
+  }
+
+  {
+    name:"George Clooney",
+    photo:"http://worldversus.com/img/george-clooney.jpg",
+    scores:[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  }
 ];
 
 module.exports=friends;
