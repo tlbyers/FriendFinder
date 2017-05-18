cacff3ba740731210bdf97f08a493094c957b105
Terri Byers <Terri Byers>
2017-05-17 22:38:14 -0400

    updated home.html

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,10 +4,10 @@ module.exports = function (app){
 
 	app.get("/api/friends", function(req,res){
 		res.json(friends);
-	})
-}
+	});
 
 	app.post("/api/friends", function(req,res){
 		friends.push(req.body);
 		res.json(true);
 });
+};
